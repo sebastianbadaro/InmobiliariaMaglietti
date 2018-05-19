e52a6ac899260febe2cee8760a6e62397f124f56
Sebastian Badaro <sebastianbadaro@gmail.com>
2018-05-19 13:00:07 -0300

    cambios varios

--- html/index.php
+++ html/index.php
@@ -2,13 +2,112 @@
 <html>
   <head>
     <meta charset="utf-8">
+
+<meta name="viewport" content="width=device-width, initial-scale=1.0">
+<script src="https://code.jquery.com/jquery-3.3.1.slim.min.js" integrity="sha384-q8i/X+965DzO0rT7abK41JStQIAqVgRVzpbzo5smXKp4YfRvH+8abtTE1Pi6jizo" crossorigin="anonymous"></script>
+  <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.14.3/umd/popper.min.js" integrity="sha384-ZMP7rVo3mIykV+2+9J3UJ46jBk0WLaUAdn689aCwoqbBJiSnjAK/l8WvCWPIPm49" crossorigin="anonymous"></script>
+  <script src="https://stackpath.bootstrapcdn.com/bootstrap/4.1.1/js/bootstrap.min.js" integrity="sha384-smHYKdLADwkXOn1EmN1qk/HfnUcbVRZyYmZ4qpPea6sjB/pTJ0euyQp0Mk8ck+5T" crossorigin="anonymous"></script>
+  <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.1.1/css/bootstrap.min.css" integrity="sha384-WskhaSGFgHYWDcbwN70/dfYBj47jz9qbsMId/iRN3ewGhXQFZCSftd1LZCfmhktB" crossorigin="anonymous">
+
+    <link rel="stylesheet" href="css/style.css">
+
     <title></title>
   </head>
   <body>
 
-<?php
-echo 'hola mundo';
- ?>
+    <nav class="navbar navbar-expand-md navbar-light bg-light my-navbar">
+        <a class="navbar-brand titulo" href="#">COOL E-Commerce</a>
+        <button class="navbar-toggler my-toggle-button" type="button" data-toggle="collapse" data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
+          <span class="my-menu">MENU</span>
+          <span class="ion-navicon-round my-menu"></span>
+        </button>
+
+        <div class="collapse navbar-collapse " id="navbarSupportedContent">
+          <ul class="navbar-nav ml-auto">
+            <li class="nav-item active">
+              <a class="nav-link" href="#">HOME <span class="sr-only">(current)</span></a>
+            </li>
+            <li class="nav-item active">
+              <a class="nav-link" href="#">ABOUT<span class="sr-only">(current)</span></a>
+            </li>
+            <li class="nav-item active">
+              <a class="nav-link" href="#">FAQ <span class="sr-only">(current)</span></a>
+            </li>
+            <li class="nav-item active">
+              <a class="nav-link" href="#">LOGIN <span class="sr-only">(current)</span></a>
+            </li>
+          </ul>
+
+        </div>
+      </nav>
+
+<section class="profile-section">
+
+    <img src="images/e-commerce.png"  class="profile-image" >
+    <h1>COOL E-Commerce</h1>
+    <ion-icon name="twitter"></ion-icon>
+    <p>The place where you will find that thing you were looking for...</p>
+</section>
+
+<section class="popular-items">
+
+
+<h3 class="title">Most popular items</h3>
+
+<div class="row align-items-center d-flex justify-content-around">
+
+        <article class="col-12 col-md-6 col-lg-4 col-xl-3 row">
+          <img src="images/submarine.png" alt="submarine">
+        </article>
+        <article class="col-12 col-md-6 col-lg-4 col-xl-3 row">
+            <img src="images/cabin.png" alt="submarine">
+        </article>
+        <article class="col-12 col-md-6 col-lg-4 col-xl-3 row">
+        <img src="images/cake.png" alt="submarine">
+        </article>
+        <article class="col-12 col-md-6 col-lg-4 col-xl-3 row">
+        <img src="images/circus.png" alt="submarine">
+        </article>
+        <article class="col-12 col-md-6 col-lg-4 col-xl-3 row">
+        <img src="images/game.png" alt="submarine">
+        </article>
+        <article class="col-12 col-md-6 col-lg-4 col-xl-3 row">
+          <img src="images/submarine.png" alt="submarine">
+        </article>
+        <article class="col-12 col-md-6 col-lg-4 col-xl-3 row">
+            <img src="images/cabin.png" alt="submarine">
+        </article>
+        <article class="col-12 col-md-6 col-lg-4 col-xl-3 row">
+        <img src="images/cake.png" alt="submarine">
+        </article>
+        <article class="col-12 col-md-6 col-lg-4 col-xl-3 row">
+        <img src="images/circus.png" alt="submarine">
+        </article>
+        <article class="col-12 col-md-6 col-lg-4 col-xl-3 row">
+        <img src="images/game.png" alt="submarine">
+        </article>
+        <article class="col-12 col-md-6 col-lg-4 col-xl-3 row">
+          <img src="images/submarine.png" alt="submarine">
+        </article>
+        <article class="col-12 col-md-6 col-lg-4 col-xl-3 row">
+            <img src="images/cabin.png" alt="submarine">
+        </article>
+</div>
+
+
+</section>
+
+<footer>
+
+
+<span class="ion-social-linkedin-outline"></span>
+<span class="ion-social-facebook-outline"></span>
+<span class="ion-social-instagram-outline"></span>
+<span class="ion-social-youtube-outline"></span>
+<span class="ion-social-twitter-outline"></span>
+
+</footer>
 
   </body>
+
 </html>
